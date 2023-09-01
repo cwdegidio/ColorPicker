537e8f58e78b30432ce4d77414cd5706b9d83aff
Curtis W. DeGidio <curtis.degidio@gmail.com>
2023-08-31 22:00:11 -0500

    Initial Commit

---
+++ ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Curtis DeGidio on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
