939f24b4dd1e8cd14bae7eda0ef11d874b45214a
Curtis W. DeGidio <curtis.degidio@gmail.com>
2023-09-01 02:34:32 -0500

    - created slider group - created title view - created color visualizer view - created button view

--- ColorPicker/ContentView.swift
+++ ColorPicker/ContentView.swift
@@ -8,14 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let startingColorSliderValue = 255.0 / 2.0
+    @State private var redSliderValue: Double = 255.0 / 2.0
+    @State private var greenSliderValue: Double = 255.0 / 2.0
+    @State private var blueSliderValue: Double = 255.0 / 2.0
+    
+    init() {
+        self.redSliderValue = startingColorSliderValue
+        self.greenSliderValue = startingColorSliderValue
+        self.blueSliderValue = startingColorSliderValue
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            title
+            colorVisualizer
+            sliderStack
+            setButton
         }
         .padding()
+    }
+    
+    var title: some View {
+        Text("Color Picker")
+            .font(.largeTitle)
+    }
+    
+    var colorVisualizer: some View {
+        RoundedRectangle(cornerRadius: 0)
+            .fill(Color(red: redSliderValue / 255.0, green: greenSliderValue / 255.0, blue: blueSliderValue / 255.0))
+    }
+    
+    var setButton: some View {
+        Button(action: {}, label: {
+            Text("Set Color")
+        })
+    }
+    
+    var sliderStack: some View {
+        Group {
+            VStack {
+                Text("Red")
+                HStack {
+                    let roundedRedValue = Int(redSliderValue.rounded())
+                    Slider(value: $redSliderValue, in: 1.0...255.0)
+                    Text("\(roundedRedValue)")
+                }
+            }
+            VStack {
+                Text("Green")
+                HStack {
+                    let roundedGreenValue = Int(greenSliderValue.rounded())
+                    Slider(value: $greenSliderValue, in: 1.0...255.0)
+                    Text("\(roundedGreenValue)")
+                }
+            }
+            VStack {
+                Text("Blue")
+                HStack {
+                    let roundedBlueValue = Int(blueSliderValue.rounded())
+                    Slider(value: $blueSliderValue, in: 1.0...255.0)
+                    Text("\(roundedBlueValue)")
+                }
+            }
+        }
     }
 }
 
